e1e662cb9d0ca2ec88bf0d84d961052ddc14e41e
Cristian Antony <cristian.buendia.m113@gmail.com>
2021-10-25 18:22:35 -0500

    roles agrega y elimina

--- src/service/RamService.js
+++ src/service/RamService.js
@@ -16,16 +16,20 @@ return axios.get(seleccionaUrl  + '/' + pRam.idRam).then(response  =>  response.
 }
 
 agregaRam (pRam) {
-let agregaUrl = '/expediente/tblSentidosSentencias/agregaTblSentidosSentencias';
-return axios.post(agregaUrl, pRam).then(response  =>  response.data);
-}
+    let agregaUrl = 'https://backliz1.herokuapp.com/ram';
+    console.log(pRam);
+    const params = new URLSearchParams()
+    params.append('tipo',pRam.tipo)
+    params.append('capacidad',pRam.capacidad)
+    params.append('medida',pRam.medida)
+    const config = { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } }
+    return axios.post(agregaUrl, params,config).then(response  =>  response.data);
+    }
 
 eliminaRam (pRam) {
-let eliminaUrl = '/expediente/tblSentidosSentencias/eliminaTblSentidosSentencias'; //Modificar
-axios.delete(eliminaUrl  + '/' + pRam.idRam, {
-headers: {'Content-Type': 'application/json;charset=UTF-8'},
-data: pRam
-});
+    let eliminaUrl = 'https://backliz1.herokuapp.com/ram'; //Modificar
+    console.log("asda ",pRam.ram_id);
+    return axios.delete(eliminaUrl + '/' + pRam.ram_id);
 
 }
 
