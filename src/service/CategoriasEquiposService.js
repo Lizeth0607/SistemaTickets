e04dd756e1dc402a2cad99ccdfe8284ef13d89c3
Cristian Antony <cristian.buendia.m113@gmail.com>
2021-10-25 16:04:01 -0500

    ya agrega CategoriasEquipos

--- src/service/CategoriasEquiposService.js
+++ src/service/CategoriasEquiposService.js
@@ -1,56 +1,43 @@
-
 import axios from 'axios';
 
 
 
-export default class  CategoriasService {
+export default class CategoriasService {
 
-/*obtenerCategoria (pCriterio){
-let buscaUrl = '/expediente/tblSentidosSentencias/buscaTblSentidosSentencias/';//Modificar
-let oValor = pCriterio.trim() === '' ? '%20' : pCriterio.trim(); 
-return axios.get(buscaUrl + oValor).then(response  =>  response.data);
+
+    obtenerCategoria() {
+        return axios.get("https://backliz1.herokuapp.com/category").then(res => res.data);
+    }
+
+
+    seleccionaCategoria(pCategorias) {
+        let seleccionaUrl = 'https://backliz1.herokuapp.com/category'; //Modificar
+        console.log(pCategorias.categoria_id);
+        return axios.get(seleccionaUrl + '/' + pCategorias.categoria_id).then(response => response.data);
+    }
+
+    agregaCategoria(pCategorias) {
+        let agregaUrl = 'https://backliz1.herokuapp.com/category';
+        const params = new URLSearchParams()
+        params.append('nombre', pCategorias.nombre)
+        params.append('descripcion', pCategorias.descripcion)
+        const config = { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } }
+        return axios.post(agregaUrl, params, config).then(response => response.data);
+    }
+
+    actualizaCategoria(pCategorias) {
+        let actualizaUrl = 'https://backliz1.herokuapp.com/category'; //Modificar
+        const params = new URLSearchParams()
+        params.append('nombre', pCategorias.nombre)
+        params.append('descripcion', pCategorias.descripcion)
+        const config = { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } }
+        return axios.put(actualizaUrl + '/' + pCategorias.idCategoria, params, config).then(response => response.data);
+    }
+
+    eliminaCategoria(pCategorias) {
+        let eliminaUrl = 'https://backliz1.herokuapp.com/category'; //Modificar
+        console.log("asda ",pCategorias.categoria_id);
+        return axios.delete(eliminaUrl + '/' + pCategorias.categoria_id);
+    }
+
 }
-*/
-
-seleccionaCategoria(pCategorias) {
-let seleccionaUrl = '/expediente/tblSentidosSentencias/seleccionaTblSentidosSentencias'; //Modificar
-return axios.get(seleccionaUrl  + '/' + pCategorias.idCategiria).then(response  =>  response.data);
-}
-
-agregaCategoria (pCategorias) {
-let agregaUrl = 'https://backliz1.herokuapp.com/category';
-const params = new URLSearchParams()
-params.append('nombre', pCategorias.nombre)
-params.append('descripcion', pCategorias.descripcion)
-const config = {headers: {'Content-Type': 'application/x-www-form-urlencoded'}}
-return axios.post(agregaUrl, params, config).then(response  =>  response.data);
-}
-
-eliminaCategoria (pCategorias) {
-let eliminaUrl = '/expediente/tblSentidosSentencias/eliminaTblSentidosSentencias'; //Modificar
-axios.delete(eliminaUrl  + '/' + pCategorias.idCategiria, {
-headers: {'Content-Type': 'application/json;charset=UTF-8'},
-data: pCategorias
-});
-
-}
-
-actualizaIndicador (pCategorias) {
-let actualizaUrl = '/expediente/tblSentidosSentencias/actualizaTblSentidosSentencias'; //Modificar
-return axios.put(actualizaUrl + '/' + pCategorias.idCategiria,  pCategorias)
-.then(response  =>  response.data);
-}
-
-obtenerCategoria (){
-    /*
-    let buscaUrl = '/expediente/tblSentidosSentencias/buscaTblSentidosSentencias/';//Modificar
-    let oValor = pCriterio.trim() === '' ? '%20' : pCriterio.trim(); 
-    return axios.get(buscaUrl + oValor).then(response  =>  response.data);*/
-    return axios.get("https://backliz1.herokuapp.com/category").then(res => res.data);
-}
-
-
-
-}                
-
-
