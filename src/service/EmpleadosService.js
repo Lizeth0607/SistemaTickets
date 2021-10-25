0fe1941743e408110177dde21d72f61b5f4c8748
Cristian Antony <cristian.buendia.m113@gmail.com>
2021-10-25 16:44:06 -0500

    empleados agrega y elimina

--- src/service/EmpleadosService.js
+++ src/service/EmpleadosService.js
@@ -7,7 +7,6 @@ export default class  EmpleadosService {
 
 obtenerEmpleado (){
     return axios.get("https://backliz1.herokuapp.com/employee").then(res => res.data);
-
 }
 
 
@@ -17,16 +16,24 @@ return axios.get(seleccionaUrl  + '/' + pEmpleados.idEmpleado).then(response  =>
 }
 
 agregaEmpleado (pEmpleados) {
-let agregaUrl = '/expediente/tblSentidosSentencias/agregaTblSentidosSentencias';
-return axios.post(agregaUrl, pEmpleados).then(response  =>  response.data);
+let agregaUrl = 'https://backliz1.herokuapp.com/employee';
+console.log(pEmpleados);
+const params = new URLSearchParams()
+params.append('nombre',pEmpleados.nombre)
+params.append('telefono',pEmpleados.telefono)
+params.append('mail',pEmpleados.mail)
+params.append('puesto',pEmpleados.puesto)
+params.append('imagen',pEmpleados.imagen)
+params.append('ubicacion_id',pEmpleados.ubicacion_id)
+params.append('sede_id',pEmpleados.sede_id)
+const config = { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } }
+return axios.post(agregaUrl, params,config).then(response  =>  response.data);
 }
 
 eliminaEmpleado (pEmpleados) {
-let eliminaUrl = '/expediente/tblSentidosSentencias/eliminaTblSentidosSentencias'; //Modificar
-axios.delete(eliminaUrl  + '/' + pEmpleados.idEmpleado, {
-headers: {'Content-Type': 'application/json;charset=UTF-8'},
-data: pEmpleados
-});
+    let eliminaUrl = 'https://backliz1.herokuapp.com/employee'; //Modificar
+    console.log("asda ",pEmpleados.empleado_id);
+    return axios.delete(eliminaUrl + '/' + pEmpleados.empleado_id);
 
 }
 
